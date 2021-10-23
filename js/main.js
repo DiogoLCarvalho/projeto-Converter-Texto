fbe926cbb9f4316b016b962dd8596d0277d745fe
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2021-10-23 17:20:30 -0300

    Finalizando Botões

--- js/main.js
+++ js/main.js
@@ -5,3 +5,69 @@ textarea.addEventListener('input', function () {
     this.style.height = 'auto';
     this.style.height = `${this.scrollHeight}px`
 });
+
+// Botões
+const btns = document.querySelectorAll('.btn');
+let containerBtn = document.querySelector('#main__bnts');
+
+btns.forEach(element => {
+    element.addEventListener('click', function () {
+        if (textarea.value == '') {
+            alert('Seu texto está vazio, digite novamente:')
+        } else {
+            console.log(this);
+            console.log(textarea.value);
+            console.log(containerBtn.children);
+            let textoModificado;
+
+
+            let acao = this.innerHTML;
+            console.log(acao);
+
+            switch (acao) {
+                case 'Maiúscula':
+                    alert('Maiúscula');
+                    textoModificado = textarea.value.toUpperCase();
+                    textarea.value = ''
+                    textarea.value = textoModificado;
+                    break;
+                case 'Minúscula':
+                    alert('Minúscula');
+                    textoModificado = textarea.value.toLowerCase();
+                    textarea.value = ''
+                    textarea.value = textoModificado;
+                    break;
+
+                case 'Capitalizado':
+                    alert('Capitalizado');
+
+                    // Transforme em array
+                    textoModificado = textarea.value.split(' ');
+
+                    for (var i = 0; i < textoModificado.length; i++) {
+                        // Pegue a primeira letra de cada array e coloque em Maiúscula e junte com o resto
+                        textoModificado[i] = textoModificado[i].charAt(0).toUpperCase() + textoModificado[i].slice(1);
+                    }
+                    textoModificado = textoModificado.join(" ");
+
+                    textarea.value = ''
+                    textarea.value = textoModificado;
+                    break;
+
+                case 'Copiar Texto':
+                    alert('Copiar Texto');
+                    
+                    break;
+
+                case 'Limpar':
+                    alert('Limpar');
+                    textarea.value = ''
+                    break;
+                default:
+                    alert('Erro ao modificar o texto!')
+            }
+
+        }
+
+    })
+});
